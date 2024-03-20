5c8e7f0ef953759ed221a89799836016d291fe8b
Islomjon Abduganiyev <islomjonabduganiyev@Islomjons-MacBook-Air.local>
2024-03-21 04:06:49 +0500

    Complated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { Body } from './component/body';
-import { Header } from './component/header';
-import { useState } from 'react';
+import { ReactMediaRecorder } from "react-media-recorder";
+import React, { useState } from 'react';
 
-export default function App() {
-  
+const App = () => {
+  const [recordedVideo, setRecordedVideo] = useState(null);
+
+  const handleRecordingStop = (mediaBlobUrl) => {
+    setRecordedVideo(mediaBlobUrl);
+  };
+
   return (
-    <>
-    <Header/>
-    <Body/>
-    </>
-  )
-}
+    <div>
+      <ReactMediaRecorder
+        screen
+        render={({ status, startRecording, stopRecording }) => (
+          <div className="action">
+            <div className="btns">
+              <button onClick={startRecording}>Start Recording</button>
+              <button onClick={stopRecording}>Stop Recording</button>
+            </div>
+            {recordedVideo && (
+              <div>
+                <video src={recordedVideo} controls autoPlay loop />
+                <a href={recordedVideo} download="recorded-video.mp4">Download Recorded Video</a>
+              </div>
+            )}
+          </div>
+        )}
+        onStop={handleRecordingStop} // stopRecording tugmasi bosilgandan so'ng
+      />
+    </div>
+  );
+};
 
+export default App;
